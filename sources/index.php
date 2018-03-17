4520205b47d8f8010172f723304a1b15206c2dbc
Felana <felana.r@hotmail.fr>
2018-03-17 15:58:47 +0100

    Mise en places du css

--- sources/index.php
+++ sources/index.php
@@ -1,36 +1,18 @@
 <?php
- session_start();
-
-require_once("model/Database.php");
-$conn=new Database();
-$db = $conn->getConnexion();
-
-if (
-  ( isset($_GET['ctrl']) && !empty($_GET['ctrl']) ) &&
-  ( isset($_GET['action']) && !empty($_GET['action']) )
-) {
-
-    $ctrl = $_GET['ctrl'];
-    $action = $_GET['action'];
-}
-else {
-  
-    $ctrl = 'sheet';
-    $action = 'create';
-}
-if(!file_exists('./controller/' . $ctrl  . 'Controller.php')){
-  $ctrl = 'sheet';
-    $action = 'create';
-}
-require_once('./controller/' . $ctrl  . 'Controller.php');
-
-$ctrl = $ctrl . 'Controller';
-$controller = $ctrl::getInstance($db);
-if(!method_exists ( $controller , $action )){
-  $action="notFound";
-} 
-$controller->$action();
-
+  session_start();
+  ini_set("display_errors", "1");
+  require_once('model/Database.php');
+  $db = new Database();
+  $db = $db->getConnexion();
 ?>
-
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MovieBook</title>
+    <link rel="stylesheet" type="text/css" href="./styles/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./styles/style.css">
+  </head>
+  <body>
+  </body>
+</html>
