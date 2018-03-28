f18bfe5c75817f7f2bab8cc985cb3977f46df33b
Felana <felana.r@hotmail.fr>
2018-03-28 11:40:44 +0200

    Design de la connexion (histoire de... en attendant)

--- sources/view/login.php
+++ sources/view/login.php
@@ -1,16 +1,26 @@
 <div class="row">
-    <div class="login col-xs-6 col-sm-8" style='border:1px solid blue;'>
+    <div class=" title col-xs-6 col-sm-12">
+        <h1>MovieBook</h1>
+        <h2>Créez votre book en ligne</h2>
+        <!-- <p>Développé par CFA UPMC Students</p>-->
+
+    </div>
+    <div class=" title col-xs-6 col-sm-12"></div>
+    <div class="left col-xs-6 col-sm-4">
+        <!-- <p>Développé par CFA UPMC Students</p>-->
+        <img src="resources/img/plume.jpg">
+    </div>
+    <div class="login col-xs-6 col-sm-6">
         <form action="index.php?ctrl=user&action=doLogin" method="post">
-            <h1>MovieBook</h1>
-            <h2>Créez votre book en ligne</h2>
             <input type="email" name="mail" id="mail" placeholder="Identifiant" required>
             <input type="password" name="password" id="password" placeholder="Mot de passe">
             <input type="submit" value="Connexion">
             <?php
             if(isset($info)) { echo $info;}
             ?>
-            <p>Développé par CFA UPMC Students</p>
         </form>
     </div>
-    <div class="col-xs-6 col-sm-4" style='border:1px solid blue;'></div>
+    <div class="left col-xs-6 col-sm-2">
+        <!-- <p>Développé par CFA UPMC Students</p>-->
+    </div>
 </div>
