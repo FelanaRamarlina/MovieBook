6954a593eb6114547dbae7189bbace7fefa2557a
Felana <felana.r@hotmail.fr>
2018-04-02 14:32:26 +0200

    Sécurité (un peu malpropre)

--- sources/controller/UserController.class.php
+++ sources/controller/UserController.class.php
@@ -6,7 +6,6 @@ class userController {
     private $db;
 
     public function __construct($db1) {
-
         require('model/User.class.php');
         require_once('model/UserManager.class.php');
         $this->userManager = new UserManager($db1);
@@ -14,90 +13,104 @@ class userController {
     }
 
     public function login() {
-        $page = 'login';
-        require('./view/main.php');
+        if(isset($_SESSION['user'])) {
+            header('location: index.php?ctrl=sheet&action=sheets');
+        }else {
+            $page = 'login';
+            require('./view/main.php');
+        }
     }
 
     public function doLogin() {
-        if(empty($_POST['mail'])||empty($_POST['password'])) {
-            $page = "login";
-            $info = "Veuillez remplir tous les champs";
-            require('./view/main.php');
-            exit();
+        if(isset($_SESSION['user'])) {
+            header('location: index.php?ctrl=sheet&action=sheets');
         }else {
-            $this->user = new User();
+            if (empty($_POST['mail']) || empty($_POST['password'])) {
+                $page = "login";
+                $info = "Veuillez remplir tous les champs";
+                require('./view/main.php');
+                exit();
+            } else {
+                $this->user = new User();
 
-            $dologinQuery = "SELECT mail, password,admin FROM users WHERE mail = :mail and password = :password";
+                $dologinQuery = "SELECT mail, password,admin FROM users WHERE mail = :mail and password = :password";
 
-            $req = $this->db->prepare($dologinQuery);
-            $req->execute([
+                $req = $this->db->prepare($dologinQuery);
+                $req->execute([
                     'mail' => $_POST['mail'],
                     'password' => md5($_POST['password'])
-            ]);
+                ]);
 
-            while ($donnees = $req->fetch())
-            {
-                $login = $donnees['mail'];
-                $password = $donnees['password'];
-                $admin = $donnees['admin'];
-            }
+                while ($donnees = $req->fetch()) {
+                    $login = $donnees['mail'];
+                    $password = $donnees['password'];
+                    $admin = $donnees['admin'];
+                }
 
 
-            if (isset($login) && isset($password)) {
-                $_SESSION['user'] = $login;
-                //$page="fiches";
-                header('location: index.php?ctrl=sheet&action=sheets');
-            }else {
-                $info = "Identifiants incorrects.";
-                $page = "login";
-                //header('location: index.php?ctrl=user&action=login');
-                require('./view/main.php');
+                if (isset($login) && isset($password)) {
+                    $_SESSION['user'] = $login;
+                    //$page="fiches";
+                    header('location: index.php?ctrl=sheet&action=sheets');
+                } else {
+                    $info = "Identifiants incorrects.";
+                    $page = "login";
+                    //header('location: index.php?ctrl=user&action=login');
+                    require('./view/main.php');
+                }
             }
         }
     }
 
     public function createUser() {
-        $page = "createUser";
-        require('./view/main.php');
+        if(isset($_SESSION['user'])) {
+            header('location: index.php?ctrl=sheet&action=sheets');
+        }else {
+            $page = "createUser";
+            require('./view/main.php');
+        }
     }
 
     public function doCreate() {
-
-        if(empty($_POST['mail'])||empty($_POST['password'])||empty($_POST['lastname'])||empty($_POST['firstname'])) {
-            $page = "createUser";
-            $info = "Veuillez remplir tous les champs";
-            require('./view/main.php');
-            exit();
+        if(isset($_SESSION['user'])) {
+            header('location: index.php?ctrl=sheet&action=sheets');
         }else {
-            /*On vérifie si l'adresse n'existe pas déjà*/
-            $req = "SELECT mail FROM users WHERE mail ='" . $_POST['mail'] . "'";
-            $query = $this->db->query($req);
-            while ($donnees = $query->fetch()) {
-                $mail = $donnees['mail'];
-            }
-            if (isset($mail)){
+            if (empty($_POST['mail']) || empty($_POST['password']) || empty($_POST['lastname']) || empty($_POST['firstname'])) {
                 $page = "createUser";
-                $info = "L'adresse email insérée existe déjà.";
+                $info = "Veuillez remplir tous les champs";
+                require('./view/main.php');
+                exit();
             } else {
-                $doCreateQuery = "INSERT INTO users VALUES(
-          			'',
-          			:lastname,
-          			:firstname,
-          			:mail,
-          			:password,
-          			:admin
-          		)";
-                $req = $this->db->prepare($doCreateQuery);
-                $ex = $req->execute(array(
-                    "lastname" => $_POST['lastname'],
-                    "firstname" => $_POST['firstname'],
-                    "mail" => $_POST['mail'],
-                    "password" => md5($_POST['password']),
-                    "admin" => false
-                ));
-                var_dump($ex);
-               /* $page = "default";
-                $_SESSION['user'] = $_POST['email'];*/
+                /*On vérifie si l'adresse n'existe pas déjà*/
+                $req = "SELECT mail FROM users WHERE mail ='" . $_POST['mail'] . "'";
+                $query = $this->db->query($req);
+                while ($donnees = $query->fetch()) {
+                    $mail = $donnees['mail'];
+                }
+                if (isset($mail)) {
+                    $page = "createUser";
+                    $info = "L'adresse email insérée existe déjà.";
+                } else {
+                    $doCreateQuery = "INSERT INTO users VALUES(
+                        '',
+                        :lastname,
+                        :firstname,
+                        :mail,
+                        :password,
+                        :admin
+                    )";
+                    $req = $this->db->prepare($doCreateQuery);
+                    $ex = $req->execute(array(
+                        "lastname" => $_POST['lastname'],
+                        "firstname" => $_POST['firstname'],
+                        "mail" => $_POST['mail'],
+                        "password" => md5($_POST['password']),
+                        "admin" => false
+                    ));
+                    var_dump($ex);
+                    /* $page = "default";
+                     $_SESSION['user'] = $_POST['email'];*/
+                }
             }
         }
     }
