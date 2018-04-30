c4717432e7858293356b704602304340aaaf7484
Felana <felana.r@hotmail.fr>
2018-04-30 16:29:52 +0200

    Ajout du bouton "Mon book"

--- sources/view/header.php
+++ sources/view/header.php
@@ -2,7 +2,8 @@
     <div class="col-xs-6 col-sm-4">-- LOGO --</div>
     <div class="col-xs-6 col-sm-4"></div>
     <div class="col-xs-6 col-sm-4">
-         <button onClick="window.location.href='index.php?ctrl=user&action=deconnexion'" class="connexion btn my-2 my-sm-0" type="submit">Déconnexion</button>
+        <button onClick="" class="mon-book btn my-2 my-sm-0" type="submit">Mon book</button>
+        <button onClick="window.location.href='index.php?ctrl=user&action=deconnexion'" class="connexion btn my-2 my-sm-0" type="submit">Déconnexion</button>
         <button onClick="window.location.href='#" class="inscription btn my-2 my-sm-0" type="submit">Mon profil</button>
 
     </div>
