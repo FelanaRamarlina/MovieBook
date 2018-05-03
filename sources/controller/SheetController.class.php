00e498ff6cb9dabc61f3922fe124045ccc1a0a27
Redha AISSAOUI <redha955@hotmail.fr>
2018-05-03 10:41:13 +0200

    Ajout de fiche fonctionnel via le menu
    On peut creer des fiches depuis l'appli, plus besoin de les ecrires en dur (manque que la photo)

--- sources/controller/SheetController.class.php
+++ sources/controller/SheetController.class.php
@@ -62,51 +62,39 @@ class sheetController {
 
         return self::$_instance;
     }
-
+*/
 	public function create(){
 		$page = 'createSheet';
         require('./view/main.php');
 	}
-	public function doCreate(){
-		$data=$this->test_fields();
-
-		if($data!=""){
-
-			$sheet=$this->sheet=new sheet();
-			$sheet->hydrate($data);
-
-
-			}
-		header('Location:index.php'.$loc);
-	}
-	/*public function modif(){
-		$page="updatesheet";
-		require('./view/main.php');
-	}
-
-	public function update(){
-
-		$data=$this->test_fields();
-
-		if($data){
-			foreach($data as $key=>$value){
-				$_SESSION['sheet'][$key]=$value;
-				$this->sheetManager->update($_SESSION['sheet']['id'], $key, $value);
-			}
-		}
-		$page="modificationOk";
-		require('./view/main.php');
-	}
-	*/
-    /*private function test_fields(){
-        $data=["title"=>"", "director"=>"", "date"=>"", "nationality"=>"", "synopsis"=>"", "image"=>""];
-        foreach($data as $key=>$value){
-            if(isset($_POST[$key]) && $_POST[$key]!=""){
-                $data[$key]=$_POST[$key];
-            }else{
-                return false;
+    public function doCreateSheet() {
+  
+                 
+            
+      
+                    $doCreateQuery = "INSERT INTO sheets VALUES(
+                        '',
+                        :title,
+                        :director,
+                        :date,
+                        :nationality,
+                        :synopsis,
+                        :image
+                    )";
+                    $req = $this->db->prepare($doCreateQuery);
+                    $ex = $req->execute(array(
+                        "title" => $_POST['title'],
+                        "director" => $_POST['director'],
+                        "date" => $_POST['date'],
+                        "nationality" => $_POST['nationality'],
+                        "synopsis" => $_POST['synopsis'],
+                        "image" => $_POST['image']
+                    ));
+                    header('location: index.php?ctrl=sheet&action=sheets');
+                    var_dump($ex);
+                
             }
-        }
-        return $data;
-    }*/
+
+    
+        
 }
