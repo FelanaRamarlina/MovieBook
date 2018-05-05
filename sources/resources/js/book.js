20211d180fa8af64913641e9276f6c677f3981cc
Antoine Plard <azertyzhd@gmail.com>
2018-05-05 15:49:24 +0200

    fonction ajouter

--- sources/resources/js/book.js
+++ sources/resources/js/book.js
@@ -1,16 +1,42 @@
 var app = new Vue({
    el: "#bookApp",
    data: {
-       sheets: []
+       sheets: [],
+       draft: [],
+       draftHint: true,
+       bookTitle: ""
    },
     created: function() {
-        fetch('http://localhost/MovieBook/api/Sheet.php').then(function(response) {
-            this.sheets = response;
+        fetch('http://localhost/MovieBook/api/Sheet.php', {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
+            },
+        ).then(response => {
+            if (response.ok) {
+                response.json().then(json => {
+                    this.sheets = json;
+
+                    for(let i = 0; i<this.sheets.length; i++) {
+                        this.sheets[i].id = this.sheets[i].id - 1;
+                    }
+                });
+            }
         });
     },
     methods: {
        display: function(event) {
-           console.log(JSON.stringify(this.sheets));
-       }
+           console.log(this.sheets);
+       },
+        showSynopsi: function(event) {
+           const el = event.target.id == "" ? event.target.parentElement : event.target;
+           el.lastChild.style.display = el.lastChild.style.display == "none" ? "block": "none";
+        },
+        addToDraft: function (event) {
+            const id = event.target.parentElement.id;
+            this.draft.push(this.sheets[parseInt(id)]);
+            this.draftHint = false;
+        }
     }
 });
--- sources/view/createBook.php
+++ sources/view/createBook.php
@@ -1,8 +1,31 @@
 <div class="row" id="bookApp">
     <div class="col-md-6">
         <p class="book-title">Brouillon</p>
+        <div class="row">
+            <div class="col-md-12 px-2 py-2">
+                <div class="mv-shadow px-2 py-2">
+                    <p v-show="draftHint">Cliquez sur "Ajouter" pour commencer à créer votre book.</p>
+                    <div v-for="element in draft" :id="element.id">
+                        <p>{{ element.title }}</p>
+                        <img :src="`resources/img/${element.image}`">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="col-md-6">
-        <p class="book-title" v-on:click="display">Selection des fiches</p>
+        <p class="book-title">Liste des fiches</p>
+        <div class="row">
+            <div v-for="sheet in sheets" class="col-md-6 px-2 py-2" >
+                <div class="mv-shadow px-2 py-2" :id="sheet.id">
+                    <p class="tile-title"><b>{{ sheet.title }}</b></p>
+                    <img :src=`resources/img/${sheet.image}` class="tile-img">
+                    <p class="tile-title">{{ sheet.date }}</p>
+                    <p class="tile-synopsis" v-on:click="showSynopsi">Voir le synopsis</p>
+                    <button class="btn-add-draft" v-on:click="addToDraft">Ajouter</button>
+                    <p class="tile-synopsis" style="display: none;">{{ sheet.synopsis }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
