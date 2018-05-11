351c33d3bae273776da4d9169b59063ad3152d13
Tylp <azertyzhd@gmail.com>
2018-05-11 14:02:09 +0200

    login form

--- sources/view/login.php
+++ sources/view/login.php
@@ -5,15 +5,15 @@
     <div class=" title col-xs-6 col-sm-12"></div>
     <div class="left col-xs-6 col-sm-4">
         <!-- <p>Développé par CFA UPMC Students</p>-->
-        
+
     </div>
     <div class="formLogin form col-xs-6 col-sm-6">
         <form action="index.php?ctrl=user&action=doLogin" method="post">
-            <input class="form1" type="email" name="mail" id="mail" placeholder="Identifiant" required>
+            <input class="loginForm" type="email" name="mail" id="mail" placeholder="Identifiant" required>
             <br/>
-            <input type="password" name="password" id="password" placeholder="Mot de passe">
+            <input class="loginForm" type="password" name="password" id="password" placeholder="Mot de passe">
             <br/>
-            <input type="submit" value="Connexion">
+            <input class="submitLoginForm" type="submit" value="Connexion">
             <p>Pas encore inscrit?</p><a class="create" href="index.php?ctrl=user&action=createUser">Créer un compte</a>
             <?php
             if(isset($info)) { echo $info;}
