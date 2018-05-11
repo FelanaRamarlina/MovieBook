680907d8de7b3aaa7d59735f983a4c798c74d032
Tylp <azertyzhd@gmail.com>
2018-05-11 10:04:11 +0200

    fix

--- sources/view/footer.php
+++ sources/view/footer.php
@@ -1,7 +1,9 @@
 </div>
 <?php
+if(isset($_GET['ctrl'])) {
     if($_GET['ctrl'] == "book")
         echo '<script src="./resources/js/book.js"></script>';
+}
 ?>
 <script src="./resources/js/pdfmake.min.js"></script>
 <script src="./resources/js/vfs_fonts.js"></script>
